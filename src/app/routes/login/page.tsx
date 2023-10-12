15f82f0b196aafe673349c30d4b40c81efd08ccc
akpan joseph <akpanjoseph2021@gmail..com>
2023-10-12 15:42:40 +0100

    done with login screen

--- src/app/routes/login/page.tsx
+++ src/app/routes/login/page.tsx
@@ -4,31 +4,16 @@ import { useState } from 'react'
 import {AiOutlineEyeInvisible,AiOutlineEye} from 'react-icons/ai'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {FormEvent} from '@/app/types/types'
-import { useRouter } from "next/navigation";
 
-export default function Home() {
+export default function login() {
 
   const [show,setShow] = useState(true)
-  const router = useRouter();
-  function submit(e:FormEvent){
-    e.preventDefault()
-
-    toast.success('success')
-    router.push('./routes/home')
-  }
 
   return (
     <div className="flex w-screen h-screen justify-center items-center flex-col">
-      <h1 className='font-medium text-4xl my-4'>Signup</h1>
+      <h1 className='font-medium text-4xl my-4'>Welcome Back</h1>
 
-      <form className="shadow-lg border rounded-md  w-[35%] py-10 px-8" onSubmit={submit} >
-
-        <div>
-          <div className="border-b-2 border-gray-500 px-2">
-          <input type="text" placeholder="Username " className="w-full outline-none py-4"  />
-          </div>
-        </div>
+      <form className="shadow-lg border rounded-md  w-[35%] py-10 px-8" >
 
 
         <div className="my-2">
@@ -48,13 +33,13 @@ export default function Home() {
 
 
             <div className='flex justify-center my-4 mt-4'>
-              <button className='bg-black text-white w-[60%] py-2 rounded-md'>Create Account</button>
+              <button className='bg-black text-white w-[60%] py-2 rounded-md'>Login</button>
             </div>
       </form>
 
       <div className='flex items-center space-x-2 my-4'>
-        <p>Already have an account</p>
-        <Link href={'/login'} className='underline font-bold  text-red-400'>Login</Link>
+        <p>Don't have an account</p>
+        <Link href={'/'} className='underline font-bold  text-red-400'>Create Account</Link>
         <button> </button>
       </div>
 
---
+++ src/app/types/types.ts
@@ -0,0 +1,6 @@
+export type FormEvent = React.FormEvent<HTMLFormElement>
+export type MouseEvent = React.MouseEvent<HTMLButtonElement>
+export type ChangeEvent = React.ChangeEvent<HTMLInputElement>
+
+
+// export { FormEvent, MouseEvent, ChangeEvent };
